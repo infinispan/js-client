88425f565b6cd2ac838fcc3bfda5c50f3b0ed2eb
Galder Zamarreño <galder@zamarreno.com>
2015-11-03 17:55:02 +0100

    More encapsulation, implement remove

--- spec/infinispan_spec.js
+++ spec/infinispan_spec.js
@@ -2,11 +2,16 @@ var ispn = require("../lib/infinispan");
 var Promise = require('promise');
 
 describe("Infinispan client", function() {
-  it("can put/replace/remove a key/value pair", function(done) {
+  it("can put -> get -> remove a key/value pair", function(done) {
     ispn.client(11222, "127.0.0.1")
+      .then(assertGetNotFound("key"))
+      .then(assertRemoveNotFound("key"))
       .then(assertPut("key", "value"))
-      .then(assertGet("key", "value"))
-      .catch(failed)
+      .then(assertGetFound("key", "value"))
+      .then(assertRemoveFound("key"))
+      .then(assertGetNotFound("key"))
+      .then(assertRemoveNotFound("key"))
+      .catch(failed(done))
       .finally(done);
   });
   //it("can ping server", function(done) {
@@ -20,20 +25,70 @@ describe("Infinispan client", function() {
 
 function assertPut(k, v) {
   return function(client) {
-    client.put(k, v).then(success);
-    return client
-  }
-}
-function assertGet(k, v) {
-  return function(client) {
-    client.get(k).then(function(value) {
-      expect(value).toBe(v);
+    console.log("About to call put: " + client);
+    return client.put(k, v).then(function() {
+      console.log("Put returned");
+      return client;
     });
-    return client
+    //client.put(k, v).then(success, failed);
+    //return client
   }
 }
 
-var success = function() {};
-var failed = function(error) {
-  expect(error.message).toBeUndefined();
+function assertGetFound(k, v) {
+  return function(client) {
+    console.log("About to call get: " + client);
+    return client.get(k).then(function(value) {
+      expect(value).toBe(v);
+      return client;
+    });
+  }
+}
+
+function assertGetNotFound(k) {
+  return function(client) {
+    console.log("About to call get: " + client);
+    return client.get(k).then(function(value) {
+      expect(value).toBeUndefined();
+      return client;
+    });
+  }
+}
+
+function assertRemoveFound(k) {
+  return function(client) {
+    console.log("About to call remove: " + client);
+    return client.remove(k).then(function(removed) {
+      console.log("Remove returned: " + removed);
+      expect(removed).toBeTruthy();
+      return client;
+    });
+    //client.put(k, v).then(success, failed);
+    //return client
+  }
+}
+
+function assertRemoveNotFound(k) {
+  return function(client) {
+    console.log("About to call remove: " + client);
+    return client.remove(k).then(function(removed) {
+      console.log("Remove returned: " + removed);
+      expect(removed).toBeFalsy();
+      return client;
+    });
+    //client.put(k, v).then(success, failed);
+    //return client
+  }
+}
+
+var failed = function(done) {
+  return function(error) {
+    done(error);
+  };
 };
+
+//var failed = function(error) {
+//  console.log(error);
+//  throw new Error(error);
+//  //expect(error.message).toBeUndefined();
+//};
--- spec/utils_spec.js
+++ spec/utils_spec.js
@@ -4,13 +4,8 @@ var utils = require("../lib/utils");
 describe("A counter", function() {
   it("can be incremented", function() {
     var counter = utils.counter(0);
-    var incremented = utils.incr(counter);
-    //console.log("Incremented: " + incremented);
-    //console.log("Incremented: " + counter._incr());
-    console.log(counter._value);
-    counter._value = 5;
-    //console.log("Incremented: " + utils.incr(counter));
-    //console.log("Incremented: " + utils.update(counter));
-    //console.log("Incremented: " + counter.update(function (n) { return n + 1 }));
+    expect(counter.incr()).toBe(1);
+    expect(counter.incr()).toBe(2);
+    expect(counter.incr()).toBe(3);
   });
 });
