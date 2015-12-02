db80f010d3eecddd1840566441acc68f0414c552
Galder Zamarreño <galder@zamarreno.com>
2015-12-02 10:01:46 +0100

    Refactor protocol to allow choosing

---
+++ spec/protocols_spec.js
@@ -0,0 +1,13 @@
+//var protocols = require('../lib/protocols');
+//
+//// TODO: Sort out once counter has been constructed
+//describe('Protocols', function() {
+//  it('can be created', function() {
+//    var hole = protocols.hole(5);
+//    hole.setValue(10);
+//
+//    //var p = protocols.version23();
+//    //p.putId();
+//    //p.decodeHeader();
+//  });
+//});
