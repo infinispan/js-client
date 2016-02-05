6885c57aa752e7b37221144f90ef3d21cd01aa8c
Galder Zamarreño <galder@zamarreno.com>
2016-02-05 15:04:03 +0100

    Add script to launch multiple Infinispan Servers
    * Currently it assumes that Infinispan Server is installed in
    /opt/infinispan-server
    * Eventually the server home will be retrieved from an environment
      variable.
    * Starts a local server and two clustered instances which have been
      created as copies of the original Infinispan Server.
    * When the script is terminated, e.g. Ctrl-C, the temporary copies are
      destroyed.
    * To execute, it requires stack to be installed, and some haskell
      packages. The first line of the script indicates the command to run to
      install the haskell packages once stack is installed.

---
+++ start.hs
@@ -0,0 +1,57 @@
+#!/usr/bin/env stack
+-- stack --install-ghc runghc --package turtle
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import Turtle
+import Control.Concurrent.Async
+
+type PortOffset = Int
+type NodeName = Text
+
+-- TODO: Get Infinispan Home from environment variable, e.g. ISPN_HOME or JBOSS_HOME
+ispnHome = "/opt/infinispan-server"
+ispnSh = "/infinispan-server/bin/standalone.sh"
+clusteredStandaloneSh = "/infinispan-server/bin/standalone.sh -c clustered.xml"
+clusterOpts = "-Djboss.node.name="%s%" \
+ \-Djboss.socket.binding.port-offset="%d%" \
+ \-Djgroups.join_timeout=1000"
+
+mkTmpDir :: Text -> Shell Turtle.FilePath
+mkTmpDir s = using (mktempdir "/tmp" s)
+
+cpR :: Text -> Turtle.FilePath -> Text
+cpR src dst = format ("cp -r "%s%" "%fp%"") src dst
+
+exec :: MonadIO io => Text -> io ExitCode
+exec cmd = shell cmd empty
+
+startServer :: Turtle.FilePath -> Shell (Async ExitCode)
+startServer h = using $ fork $ exec ((format fp h) <> ispnSh)
+
+startClusterServer :: Turtle.FilePath -> Text -> Shell (Async ExitCode)
+startClusterServer h ps = using $ fork $ exec ((format fp h) <> clusteredStandaloneSh <> " " <> ps)
+
+launchLocalNode :: Shell (Async ExitCode)
+launchLocalNode = do
+    dir <- mkTmpDir "local"
+    _   <- exec (cpR ispnHome dir)
+    startServer dir
+
+mkClusterOpts :: NodeName -> PortOffset -> Text
+mkClusterOpts n p = format (clusterOpts) n p
+
+launchClusterNode :: NodeName -> PortOffset -> Shell (Async ExitCode)
+launchClusterNode n p = do
+    dir <- mkTmpDir "cluster"
+    _   <- exec (cpR ispnHome dir)
+    startClusterServer dir (mkClusterOpts n p)
+
+main = sh (do
+    local      <- launchLocalNode
+    cluster1   <- launchClusterNode "node1" 100
+    cluster2   <- launchClusterNode "node2" 200
+    -- TODO: Check that cluster forms
+    _ <- liftIO (wait local)
+    _ <- liftIO (wait cluster1)
+    liftIO (wait cluster2))
