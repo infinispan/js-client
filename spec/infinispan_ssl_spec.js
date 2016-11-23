02a9417bf573e217d3e3294ae272f1f4c14a3389
Galder Zamarreño <galder@zamarreno.com>
2016-11-23 15:52:14 +0100

    HRJS-8 Add pfx/p12 crypto store support

--- spec/infinispan_ssl_spec.js
+++ spec/infinispan_ssl_spec.js
@@ -1,3 +1,5 @@
+var util = require('util');
+
 var t = require('./utils/testing'); // Testing dependency
 
 describe('Infinispan TLS/SSL client', function() {
@@ -5,48 +7,52 @@ describe('Infinispan TLS/SSL client', function() {
   // All invocations needs to be directed to localhost instead of 127.0.0.1
   // because Node.js uses `localhost` as default server name if none provided.
 
-  it('can operate on data via trusted encrypted transport', function(done) {
-    t.client({port: 11422, host: 'localhost'}, sslTrustOpts())
-      .then(t.assert(t.put('ssl-trust-key', 'ssl-trust-value')))
-      .then(t.assert(t.get('ssl-trust-key'), t.toBe('ssl-trust-value')))
-      .then(t.disconnect())
-      .catch(t.failed(done))
-      .finally(done);
-  });
+  it('can operate on data via trusted encrypted transport',
+     testSsl('trust', 11422, sslTrust())
+  );
 
-  it('can operate on data via authenticated encrypted transport', function(done) {
-    t.client({port: 11432, host: 'localhost'}, sslAuthOpts())
-      .then(t.assert(t.put('ssl-auth-key', 'ssl-auth-value')))
-      .then(t.assert(t.get('ssl-auth-key'), t.toBe('ssl-auth-value')))
-      .then(t.disconnect())
-      .catch(t.failed(done))
-      .finally(done);
-  });
+  it('can operate on data via crypto store trusted encrypted transport',
+     testSsl('trust-cryptostore', 11422, sslTrustCryptoStore())
+  );
 
-  it('can operate on data via SNI trusted encrypted transport', function(done) {
-    t.client({port: 11442, host: 'localhost'}, sslSniTrustedOpts())
-      .then(t.assert(t.put('ssl-sni-trusted-key', 'ssl-sni-trusted-value')))
-      .then(t.assert(t.get('ssl-sni-trusted-key'), t.toBe('ssl-sni-trusted-value')))
-      .then(t.disconnect())
-      .catch(t.failed(done))
-      .finally(done);
-  });
+  it('can operate on data via authenticated encrypted transport',
+     testSsl('auth', 11432, sslAuth())
+  );
 
-  it('fails to operate if default server name (SNI) does not match default server realm', function(done) {
-    t.client({port: 11442, host: 'localhost'}, sslSniDefaultOpts())
-      .then(shouldFail())
-      .catch(expectError("Hostname/IP doesn't match certificate's altnames"))
-      .finally(done);
-  });
+  it('can operate on data via SNI trusted encrypted transport',
+     testSsl('sni-trusted', 11442, sslSniTrusted())
+  );
 
-  it('fails to operate if server name (SNI) and trusted certificate are incorrect', function(done) {
-    t.client({port: 11442, host: 'localhost'}, sslSniUntrustedOpts())
-      .then(shouldFail())
-      .catch(expectError('CERT_SIGNATURE_FAILURE'))
-      .finally(done);
-  });
+  it('fails to operate if default server name (SNI) does not match default server realm',
+     testError("Hostname/IP doesn't match certificate's altnames", sslSniDefault())
+  );
 
-  function sslTrustOpts() {
+  it('fails to operate if server name (SNI) and trusted certificate are incorrect',
+     testError('CERT_SIGNATURE_FAILURE', sslSniUntrusted())
+  );
+
+  it('fails to operate if no passphrase provided for crypto store',
+     testError('No passphrase defined for crypto store', sslStoreNoPassphrase())
+  );
+
+  it('fails to operate if no path provided for crypto store',
+     testError('No path defined for crypto store', sslStoreNoPath())
+  );
+
+  function testSsl(infix, port, sslOpts) {
+    var k = util.format('ssl-%s-key', infix);
+    var v = util.format('ssl-%s-value', infix);
+    return function(done) {
+      t.client({port: port, host: 'localhost'}, sslOpts)
+        .then(t.assert(t.put(k, v)))
+        .then(t.assert(t.get(k), t.toBe(v)))
+        .then(t.disconnect())
+        .catch(t.failed(done))
+        .finally(done);
+    }
+  }
+
+  function sslTrust() {
     return {
       ssl: {
         enabled: true,
@@ -55,7 +61,19 @@ describe('Infinispan TLS/SSL client', function() {
     }
   }
 
-  function sslAuthOpts() {
+  function sslTrustCryptoStore() {
+    return {
+      ssl: {
+        enabled: true,
+        cryptoStore: {
+          path: 'spec/ssl/trust/p12/truststore_client.p12',
+          passphrase: 'secret'
+        }
+      }
+    }
+  }
+
+  function sslAuth() {
     return {
       ssl: {
         enabled: true,
@@ -69,7 +87,7 @@ describe('Infinispan TLS/SSL client', function() {
     }
   }
 
-  function sslSniTrustedOpts() {
+  function sslSniTrusted() {
     return {
       ssl: {
         enabled: true,
@@ -79,7 +97,7 @@ describe('Infinispan TLS/SSL client', function() {
     }
   }
 
-  function sslSniDefaultOpts() {
+  function sslSniDefault() {
     return {
       ssl: {
         enabled: true,
@@ -88,13 +106,22 @@ describe('Infinispan TLS/SSL client', function() {
     }
   }
 
-  function sslSniUntrustedOpts() {
+  function sslSniUntrusted() {
     return {
       ssl: {
         enabled: true,
         trustCerts: ['spec/ssl/sni/untrusted/client/untrusted-root-ca.crt.pem'],
         sniHostName: "untrusted.acme"
       }
+    }
+  }
+
+  function testError(err, sslOpts) {
+    return function(done) {
+      t.client({port: 11442, host: 'localhost'}, sslOpts)
+        .then(shouldFail())
+        .catch(expectError(err))
+        .finally(done);
     }
   }
 
@@ -110,6 +137,26 @@ describe('Infinispan TLS/SSL client', function() {
   function expectError(msg) {
     return function(err) {
       expect(err.message).toBe(msg);
+    }
+  }
+
+  function sslStoreNoPassphrase() {
+    return {
+      ssl: {
+        enabled: true,
+        cryptoStore: {
+          path: 'spec/ssl/trust/p12/truststore_client.p12'
+        }
+      }
+    }
+  }
+
+  function sslStoreNoPath() {
+    return {
+      ssl: {
+        enabled: true,
+        cryptoStore: {}
+      }
     }
   }
 
