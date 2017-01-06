7bf8575a808c4e4a104ee60db890c4c86416f430
Galder Zamarreño <galder@zamarreno.com>
2017-01-06 10:23:58 +0100

    HRJS-18 Remove paralell iteration examples
    * Parallel iteration is not a pattern that we promote since you can
      never be sure how many parallel iterations should be done.
    * Instead, The normal pattern is to get 1 or N entries, depending on the
      batch size, and check whether done to ask more entries.
    * This should avoid the spurious NoSuchElement exceptions received from
      server.

--- spec/tests.js
+++ spec/tests.js
@@ -12,16 +12,16 @@ exports.execPutGet = function(path, prefix, client, expectFun) {
   }
 };
 
- exports.iterateEntries = function(prefix, client) {
+ exports.iterateEntries = function(prefix, batchSize, client) {
   return function(done) {
     var pairs = [
       {key: prefix + '-it1', value: 'v1', done: false},
       {key: prefix + '-it2', value: 'v2', done: false},
       {key: prefix + '-it3', value: 'v3', done: false}];
     client
+      .then(t.assert(t.clear()))
       .then(t.assert(t.putAll(pairs), t.toBeUndefined))
-      .then(t.parIterator(1, pairs)) // Iterate all data, 1 element at time, parallel
-      .then(t.seqIterator(3, pairs)) // Iterate all data, 3 elements at time, sequential
+      .then(t.seqIterator(batchSize, pairs))
       .catch(t.failed(done))
       .finally(done);
   }
