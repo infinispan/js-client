2ceb2b0da9673b2e03d4a1616a43398ef5249561
Galder Zamarreño <galder@zamarreno.com>
2019-05-03 10:53:04 +0200

    Run testsuite with node instead of jasmine-node
    * Location of jasmine-node can vary between node.js versions,
    so simply use node instead.

--- run-testsuite.sh
+++ run-testsuite.sh
@@ -3,4 +3,4 @@
 # Stop if any errors found
 set -e
 
-./node_modules/.bin/jasmine-node spec --captureExceptions
+node --trace-deprecation node_modules/jasmine-node/lib/jasmine-node/cli.js spec/* --captureExceptions
