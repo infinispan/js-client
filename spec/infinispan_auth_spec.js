c4291fe1017327830b50a3544c97548701dc23ba
Katia Aresti <karesti@redhat.com>
2021-05-25 14:48:05 +0200

    HRJS-36 SASL Authentication
    Co-authored-by: Tristan Tarrant <ttarrant@redhat.com>

--- spec/infinispan_auth_spec.js
+++ spec/infinispan_auth_spec.js
@@ -1,40 +1,19 @@
 var t = require('./utils/testing'); // Testing dependency
 
-describe('Infinispan local client', function() {
-    it('PLAIN', function(done) {t.client(t.local, {authentication: {
-            enabled: true,
-            saslMechanism: 'PLAIN',
-            userName: 'admin',
-            password: 'pass'
-        }})
-        .then(t.assert(t.put('key', 'value')))
-        .then(t.disconnect())
-        .catch(t.failed(done))
-        .finally(done);
-    });
-
-    // it('SCRAM-SHA-512', function(done) {t.client(t.local, {authentication: {
-    //         enabled: true,
-    //         saslMechanism: 'SCRAM-SHA-1',
-    //         userName: 'admin',
-    //         password: 'pass'
-    //     }})
-    //   .then(t.assert(t.put('key', 'value')))
-    //   .then(t.disconnect())
-    //   .catch(t.failed(done))
-    //   .finally(done);
-    // });
-
-    // it('DIGEST-MD5', function(done) {t.client(t.local, {authentication: {
-    //         enabled: true,
-    //         saslMechanism: 'DIGEST-MD5',
-    //         userName: 'admin',
-    //         password: 'pass'
-    //     }})
-    //     .then(t.assert(t.put('key', 'value')))
-    //     .then(t.disconnect())
-    //     .catch(t.failed(done))
-    //     .finally(done);
-    // });
-
+describe('Infinispan local client', function () {
+  [ 'SCRAM-SHA-1', 'SCRAM-SHA-256', 'SCRAM-SHA-384', 'SCRAM-SHA-512', 'PLAIN', 'DIGEST-MD5'].forEach(m => 
+  it(m, function (done) {
+    t.client(t.local, {
+      authentication: {
+        enabled: true,
+        saslMechanism: m,
+        userName: 'admin',
+        password: 'pass'
+      }
+    })
+    .then(t.assert(t.put('key', 'value')))
+    .then(t.disconnect())
+    .catch(t.failed(done))
+    .finally(done);
+  }));
 });
