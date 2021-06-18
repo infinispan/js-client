5202f1ddc2027273d8605f3254f3d14766553c96
Gustavo Lira <guga.java@gmail.com>
2021-06-18 16:44:18 -0300

    Generate the JUnit XML report Increase smoke tests coverage Override ispn SERVER_VERSION and DOWNLOAD_URL

--- smoke-tests.sh
+++ smoke-tests.sh
@@ -6,9 +6,15 @@ node \
   --trace-deprecation \
   node_modules/jasmine-node/lib/jasmine-node/cli.js \
   spec/codec_spec.js \
+  spec/functional_spec.js \
+  spec/infinispan_auth_spec.js \
   spec/infinispan_cluster_spec.js \
-  spec/infinispan_local_spec.js \
+  spec/infinispan_expiry_spec.js \
   spec/infinispan_json_spec.js \
+  spec/infinispan_local_spec.js \
   spec/infinispan_ssl_spec.js \
+  spec/infinispan_stress_spec.js \
   spec/protocols_spec.js \
-  --captureExceptions
+  spec/tests.js \
+  spec/utils_spec.js \
+  --captureExceptions --junitreport --forceexit
